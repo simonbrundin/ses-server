0d717cec477d79142ca7200f27ab5407f0d85d44
Simon Brundin <simonbrundin@gmail.com>
2020-08-02 01:39:56 +0200

    skapade enkel server

--- server.js
+++ server.js
@@ -1,2 +1,16 @@
 const express = require('express');
 const app = express();
+
+app.listen(3000);
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(req.headers);
+  next();
+})
+
+app.get('/', (req, res) => {
+  res.send('Startsida');
+});
