837c52f0a1746dba15c088c30706b3dd3af7354c
Simon Brundin <54498413+simonbrundin@users.noreply.github.com>
2020-08-02 16:59:40 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.listen(3000);
+app.listen(process.env.PORT || 5000);
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
