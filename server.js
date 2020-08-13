2dd554fe964b504d22fd3599c4added8f96bdc04
Simon Brundin <simonbrundin@gmail.com>
2020-08-13 14:17:35 +0200

    Kopplade ihop och testade kopplingen till databasen på Heroku

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const app = express();
-
+var knex = require("knex")({
+  client: "pg",
+  connection: {
+    host: "ec2-54-228-209-117.eu-west-1.compute.amazonaws.com",
+    user: "gjedfoxspsphbk",
+    password:
+      "5b932fe02e6e00f011d1d99402f7b9c20563b7a48b65c57bd2874a83047f82bb",
+    database: "d5b9uiqv8vquho",
+    ssl: true
+  },
+});
 app.listen(process.env.PORT || 5000);
 
 app.use(express.urlencoded({ extended: false }));
@@ -14,3 +24,13 @@ app.use((req, res, next) => {
 app.get('/', (req, res) => {
   res.send('Startsida');
 });
+
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
+
+
+knex
+  .select("*")
+  .from("users")
+  .then((data) => {
+    console.log(data);
+  });
