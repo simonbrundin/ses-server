29af49f9ef12279c12d787adb53fa792dcf36e70
Simon Brundin <simonbrundin@gmail.com>
2020-08-16 17:48:35 +0200

    Hittar matchande tider

--- server.js
+++ server.js
@@ -106,18 +106,71 @@ app.post('/sparaluckor', (req, res) => {
     });
 })
 
-// Hämta matchindo
+// Hämta möjliga matchtider
+
+app.post('/matchluckor', (req, res) => {
+  let hemma1 = 7;
+  let hemma2 = 5;
+  let borta1 = 10;
+  let borta2 = 13;
+  databas.select('uddaveckor', 'jämnaveckor').from('spelare').where('ID', hemma1).orWhere('ID', hemma2).orWhere('ID', borta1).orWhere('ID', borta2).then((array) => {
+
+    /* console.log(array[0]); */
+    /* console.log('hemma1: udda veckor = ' + array[0].uddaveckor);
+    console.log('hemma2: udda veckor = ' + array[1].uddaveckor);
+    console.log('borta1: udda veckor = ' + array[2].uddaveckor);
+    console.log('borta2: udda veckor = ' + array[3].uddaveckor); */
+    var intersection1 = array[0].uddaveckor.filter(function (e) {
+      return array[1].uddaveckor.indexOf(e) > -1;
+    });
+    var intersection2 = intersection1.filter(function (e) {
+      return array[2].uddaveckor.indexOf(e) > -1;
+    });
+    var intersection3 = intersection2.filter(function (e) {
+      return array[3].uddaveckor.indexOf(e) > -1;
+    });
+    if (intersection3.length < 2) {
+      let uddaluckor = [];
+      console.log('inga gemensamma luckor');
+
+    } else {
+      let convertedSlots = [];
+      for (var i = 0, len = intersection3.length; i < len; i++) {
+        let dagMotNummer = intersection3[i].replace("M", "1").replace("Ti", "2").replace("O", "3").replace("To", "4").replace("F", "5").replace("L", "6").replace("S", "7");
+        console.log(dagMotNummer);
+        let numberFromString = parseInt(dagMotNummer);
+        convertedSlots.push(numberFromString);
+      }
+      convertedSlots.sort();
+
+      for (let index = 0; index < convertedSlots.length; index++) {
+        const element = array[index];
+
+      }
+
+
+      console.log(convertedSlots);
+    }
+
+  })
+})
+
+
+// Hämta matchdata
 
 app.post('/matchdata', (req, res) => {
 
 
 
   let matchnr = req.body.matchnr;
+  console.log(matchnr);
   databas
-    .select("hemma1")
-    .from("matcher-timrå-H2-södra")
-    .where('ID', matchnr)
+    .select('hemma1, hemma2, borta1, borta2')
+    .from('spelare')
+    .join('matcher-timrå-H2-södra')
+    .where('matcher-timrå-H2-södra.ID', 1)
     .then((array) => {
+      console.log(array);
       let hemma1 = array[0].hemma1;
 
       res.json(hemma1);
