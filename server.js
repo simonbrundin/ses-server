5a35022c3cbe75f072c07042922625b317efd0c5
Simon Brundin <simonbrundin@gmail.com>
2021-03-17 12:29:57 +0100

    changed origin

--- server.js
+++ server.js
@@ -29,7 +29,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://superelitserien-server.herokuapp.com/"
+  );
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
